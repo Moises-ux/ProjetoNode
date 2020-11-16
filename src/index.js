0b2b75588ac6ac3e1ee53c185e5a92a55990ca6c
Moises Ferreira <moisesferreira447@gmail.com>
2020-11-16 16:13:08 -0300

    Criação do projeto

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const { request, response } = require('express');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+  return response.json({ mensage: "Olá Mundo" });
+})
+
+app.listen(3333);
